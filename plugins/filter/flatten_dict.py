c803eee236c937be38a16f4523d7a62371fb0cd1
AAYUSH ANAND <aayushanand407@gmail.com>
2025-05-15 07:04:46 +0530

    removed_the_dependancy_on_panda

---
+++ plugins/filter/flatten_dict.py
@@ -0,0 +1,19 @@
+def flatten_dict(data, parent_key='', sep='_'):
+    items = {}
+    for k, v in data.items():
+        new_key = f"{parent_key}{sep}{k}" if parent_key else k
+        if isinstance(v, dict):
+            items.update(flatten_dict(v, new_key, sep=sep))
+        elif isinstance(v, list):
+            for i, item in enumerate(v):
+                if isinstance(item, dict):
+                    items.update(flatten_dict(item, f"{new_key}{sep}{i}", sep=sep))
+                else:
+                    items[f"{new_key}{sep}{i}"] = str(item)
+        else:
+            items[new_key] = str(v)
+    return items
+
+class FilterModule:
+    def filters(self):
+        return {'flatten_dict': flatten_dict}
